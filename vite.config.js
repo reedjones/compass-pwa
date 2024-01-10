a4795196a2f474e20f0b0d2cd7a3d568e60220df
reedjones <reedjones760@yahoo.com>
2024-01-10 12:51:09 -0800

    Second Commit 😎

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+import {glob} from "glob";
+
 const path = require('path')
+
+
 
 export default {
     root: path.resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist'
+        outDir: path.join(__dirname, "dist"),
+        rollupOptions: {
+            input: ['./src/index.html', './src/home.html'],
+
+        },
     },
     server: {
         port: 8080
